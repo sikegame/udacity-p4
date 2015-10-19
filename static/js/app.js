5eb9f0db2ec414d392b32f5bf6acde839aa925f0
Shinsuke JJ Ikegame <shinsuke@ikega.me>
2015-10-19 21:26:47 +0700

    End of Day commit

--- static/js/app.js
+++ static/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'web-client-id',
+        CLIENT_ID: '267170457165-stmcnu96aa24kkjdf3vt92icntm99td6.apps.googleusercontent.com',
         SCOPES: 'email profile',
         signedIn: false
     }
